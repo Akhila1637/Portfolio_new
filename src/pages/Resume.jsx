d5273d2f320d494e1516845fcea6975c2d503b1e
Akhila <akhilavardholuyadav@gmail.com>
2025-04-11 12:29:36 +0530

    resume updated

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -8,8 +8,7 @@ const Resume = () => {
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold text-gray-800">My Resume</h1>
         <a
-          href="https://drive.google.com/file/d/1QdHvs-kZicojVoH6akHxu8irvJv7EA-s/preview
-          "
+          href="https://drive.google.com/file/d/1L0X0gfEB-leAU6Efa9z8-D7atnzwfZKI/preview"
           download
           className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 flex items-center"
         >
@@ -21,7 +20,7 @@ const Resume = () => {
       {/* Display PDF in iframe */}
       <div className="w-full h-[90vh] border rounded-lg shadow">
         <iframe
-          src="https://drive.google.com/file/d/1QdHvs-kZicojVoH6akHxu8irvJv7EA-s/preview"
+          src="https://drive.google.com/file/d/1L0X0gfEB-leAU6Efa9z8-D7atnzwfZKI/preview"
           title="Resume PDF"
           className="w-full h-full"
           style={{border:"none"}}
